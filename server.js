bd8a6c50433f03df84b7518131765da48160c54f
Guido Zlatar <guido_zlatar07@hotmail.com>
2022-04-07 17:07:00 -0300

    Clase 10

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-import express from 'express';
-import router from './routes/index.js';
+const express = require('express');
+//import router from './routes/index.js';
+const handlebars = require('express-handlebars');
 const app = express();
+
+
+app.engine('hbs', handlebars({
+    extname: ".hbs",
+    defaultLayout: "inicio.hbs",
+    layoutsDir: __dirname + "/views",
+    layoutsPartial: __dirname + "/views/partials"})
+    )
+    
+app.set("view engine", "hbs");
+app.set("views", "views");
+app.use(express.static('views'))
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-app.use("/api", router)
-app.use(express.static('views'))
+//app.use("/api", router)
+
+
+const productos = [
+    {price: 1000, title: "libro", thumbnail: "https://cdn3.iconfinder.com/data/icons/education-and-school-8/48/Book-256.png"},
+    {price: 80, title: "lapicera", thumbnail: "https://cdn3.iconfinder.com/data/icons/flat-icons-web/40/Sharpie-256.png"},
+    {price: 110, title: "regla", thumbnail: "https://cdn3.iconfinder.com/data/icons/education-and-learning-set-2-1/256/47-256.png"},
+    {price: 4000, title: "mochila", thumbnail: "https://cdn4.iconfinder.com/data/icons/camping-flat/614/1341_-_Bag-256.png"}
+]
+
+app.get("/", (req, res) =>{
+    res.render("index", {productos: productos})
+})
+app.get("/productos", (req, res) =>{
+    res.render("inicio", {productos: productos})
+})
+
+app.post("/productos", (req, res)=>{
+    productos.push(req.body)
+    res.redirect("/productos")
+})
 
 const PORT = 8080 
 
